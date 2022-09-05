3fe6d6f8b3d2745bb6c90a7b4dd8a37d5c217e5f
Toby <508087823@qq.com>
2022-09-05 12:25:53 +0800

    INIT PROJECT

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
+app.use(createPinia())
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import externalGlobals from 'rollup-plugin-external-globals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: [
+        'vue',
+        'axios',
+        'vue-demi',
+        'pinia',
+      ],
+      plugins: [
+        externalGlobals({
+          vue: 'Vue',
+          axios: 'Axios',
+          'vue-demi': 'VueDemi',
+          pinia: 'Pinia',
+        }),
+      ],
+    },
+  },
 })
